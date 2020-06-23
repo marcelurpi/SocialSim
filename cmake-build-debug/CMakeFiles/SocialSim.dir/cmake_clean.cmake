06742ff66f8853196b802d57d8d7a24bffad4e67
Marcel Urpí <marcelurpi@gmail.com>
2020-06-23 18:44:06 +0200

    Git + CMake Setup

---
+++ cmake-build-debug/CMakeFiles/SocialSim.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SocialSim.dir/main.cpp.o"
+  "SocialSim"
+  "SocialSim.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SocialSim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
